d40fb46ab3e34897cd0139f64108c61b5e302bcd
Stefany20 <stefanymrojasp@gmail.com>
2023-05-16 10:46:10 -0400

    primer commit

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import numpy as np
 import pandas as pd 
 import ast
 import fastapi as FastAPI
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
 
 app = FastAPI.FastAPI()
 #importamos la data 
-data = pd.read_csv("mi_movies_dataset.csv")
+data = pd.read_csv("definido_movies_dataset.csv")
 
 #Los valores nulos de los campos revenue, budget deben ser rellenados por el número 0.
 data["revenue"].fillna(0, inplace = True)
@@ -14,7 +17,7 @@ data["budget"].fillna(0, inplace = True)
 #convierto los valores de la columna budhet en valores flotantes para poder realizar la duvision ya que estaban como objetos
 data['budget'] = data['budget'].astype(float)
 
-def calcular_retorno(row):
+def calcular_retor(row):
     revenue = row['revenue']
     budget = row['budget']
     
@@ -24,19 +27,17 @@ def calcular_retorno(row):
         return 0
 
 # Crear la columna 'return' aplicando la función calcular_retorno a cada fila
-data['return'] = data.apply(calcular_retorno, axis=1)
+data['return'] = data.apply(calcular_retor, axis=1)
 
-import fastapi as FastAPI
 
-app = FastAPI.FastAPI()
 
 @app.get("/peliculas_mes/{mes}")
 def peliculas_mes(mes: str): 
-     '''Se ingresa el mes y la funcion retorna la cantidad de peliculas que se estrenaron 
+    '''Se ingresa el mes y la funcion retorna la cantidad de peliculas que se estrenaron 
         ese mes 
-    '''
-     #nos imprimira el ideoma que le indiquemos  
-     meses_espanol_ingles = {
+    ''' 
+#nos imprimira el ideoma que le indiquemos  
+    espanol_ingles = {
      'enero': 'January',
      'febrero': 'February',
      'marzo': 'March',
@@ -50,25 +51,25 @@ def peliculas_mes(mes: str):
      'noviembre': 'November',
      'diciembre': 'December'}
    
-     if mes.lower() in meses_espanol_ingles:
-        mes_mediador = meses_espanol_ingles[mes.lower()]
-     elif mes.lower not in meses_espanol_ingles:
-        mes_mediador = mes
+    if mes.lower() in espanol_ingles:
+        mes_medi = espanol_ingles[mes.lower()]
+    elif mes.lower not in espanol_ingles:
+        mes_medi = mes
         
     #Convertimos a la columna de tipo datetime
-     data["release_date"] = pd.to_datetime(data["release_date"])
+    data["release_date"] = pd.to_datetime(data["release_date"])
     
     #Obtenemos mes y año de la columna
-     data['mes'] = data['release_date'].dt.month_name() 
-     data['año'] = data['release_date'].dt.year
+    data['mes'] = data['release_date'].dt.month_name() 
+    data['año'] = data['release_date'].dt.year
 
     #Filtramos por el mes proporcionado
-     peliculas_mes = data[data['mes'].str.lower() == mes_mediador.lower()]
+    peliculas_mes = data[data['mes'].str.lower() == mes_medi.lower()]
     
     #Cantidada de pelicula por mes 
-     cantidad = len(peliculas_mes)
+    cantidad = len(peliculas_mes)
     
-     return {'mes':mes, 'cantidad':cantidad}
+    return {'mes':mes, 'cantidad':cantidad}
 
 
 @app.get("/peliculas_dia/{dia}")
@@ -79,7 +80,7 @@ def peliculas_dia(dia:str):
 
     ''' 
     #imprimira el ideoma que le indiquemos 
-    dias_espanol_ingles = {
+    espanol_ingles = {
     'lunes': 'Monday',
     'martes': 'Tuesday',
     'miércoles': 'Wednesday',
@@ -88,10 +89,10 @@ def peliculas_dia(dia:str):
     'sábado': 'Saturday',
     'domingo': 'Sunday'}
     
-    if dia.lower() in dias_espanol_ingles:
-        dia_mediador = dias_espanol_ingles[dia.lower()]
-    elif dia.lower not in dias_espanol_ingles:
-        dia_mediador = dia
+    if dia.lower() in espanol_ingles:
+        dia_medi = espanol_ingles[dia.lower()]
+    elif dia.lower not in espanol_ingles:
+        dia_medi = dia
         
     # Convertir la columna a tipo datetime 
     data['release_date'] = pd.to_datetime(data['release_date']) 
@@ -100,33 +101,33 @@ def peliculas_dia(dia:str):
     data['dia_semana'] = data['release_date'].dt.day_name()
 
     # Filtrar  por el día de la semana proporcionado
-    peliculas_dia = data[data['dia_semana'].str.lower() == dia_mediador.lower()]
+    peliculas_dia = data[data['dia_semana'].str.lower() == dia_medi.lower()]
  
     cantidad = len(peliculas_dia) 
     return  {'dia': dia, 'cantidad': cantidad}
 
 
 @app.get("/franquicia/{franquicia}")
-def franquicia(franquicia): 
-    '''Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia total y 
-    promedio
-    ''' 
-    # Filtrar  por la franquicia proporcionada
-    franquicia_data = data[data['belongs_to_collection'].notnull() & data['belongs_to_collection'].apply(lambda x: isinstance(x, str) and franquicia.lower() in x.lower())]
+def franquicia(franquicia: str):
+    '''
+    Se ingresa la franquicia, retornando la
+      cantidad de peliculas, ganancia total y promedio
+    '''
+    # Filtro el DataFrame por la franquicia proporcionada
+    franquicia_fi = data[data['belongs_to_collection'].notnull() & data['belongs_to_collection'].apply(lambda x: isinstance(x, str) and franquicia.lower() in x.lower())]
 
-    # Obtener la cantidad de películas en la franquicia
-    cantidad = len(franquicia_data)
+    cantidad = len(franquicia_fi)
 
-    # Calcular la ganancia total y el promedio de la franquicia
-    ganancia_total = franquicia_data['revenue'].sum()
-    ganancia_promedio = franquicia_data['revenue'].mean()
+    ganancia_en_total = franquicia_fi['revenue'].sum()-franquicia_fi['budget'].sum()
+    ganancia_en_promedio = ganancia_en_total/cantidad
 
-    
-    return {'franquicia':franquicia, 'cantidad': cantidad, 'ganancia_total':ganancia_total, 'ganancia_promedio':ganancia_promedio}
+    return {'franquicia': franquicia, 'cantidad': cantidad, 'ganancia_total': ganancia_en_total, 'ganancia_promedio': ganancia_en_promedio}
 
 @app.get("/peliculas_pais/{pais}")
 def peliculas_pais(pais: str):
-    
+    '''
+    Ingresas el pais, retornando la cantidad de peliculas producidas en el mismo
+    '''
     # Filtrar la data por el país proporcionado
     pelicula_pais = data[data["production_countries"].str.contains(pais, case=False, na=False)]
 
@@ -137,34 +138,34 @@ def peliculas_pais(pais: str):
     return {'pais': pais, 'cantidad': cantidad}
 
 
-@app.get("/productora/{productora}")
-def productoras(productora:str):
-    ''' 
-    Ingresas la productora, retornando la ganancia total y la cantidad de peliculas que produjero
-    ''' 
-     # Filtrar por la productora proporcionada
-    produ = data[["production_companies", "budget", "revenue"]].dropna()
-    produ["production_companies"] = produ["production_companies"].map(str.lower)
+@app.get("/retorno/{productora}")
+def productoras(productora: str):
+    '''
+    Ingresas la productora, retornando la ganancia total y 
+    la cantidad de peliculas que produjeron
+    '''
+    # Filtro por la productora proporcionada
+    productora_fil = data[data['production_companies'].notnull() & data['production_companies'].str.contains(productora, case=False)]
 
     # Obtener la ganancia total y la cantidad de películas producidas por la productora
-    cantidad = produ.shape[0]
-    ganacia_total = (produ["revenue"]- produ["budget"]).sum()
+    ganancia_en_total = productora_fil['revenue'].sum()-productora_fil['budget'].sum()
+    cantidad = len(productora_fil)
 
-    return {"productora": productora, "ganancia total": ganacia_total, "cantidad": cantidad}
+    return {'productora': productora, 'ganancia_total': ganancia_en_total, 'cantidad': cantidad}
 
 
-@app.get("/retorno/{pelicula}")
-def retorno(pelicula: str): 
-    '''Ingresas la pelicula, retornando la inversion, la ganancia, 
-    el retorno y el año en el que se lanzo
-    ''' 
-    #filtramos las filas correspondientes 
-    fila_pelicula = data[data["title"] == pelicula].iloc[0]
+@app.get("/recomendaciones/{recomendacion}")
+def recomendaciones(titulo : str):
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(data['title'])
+    cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
     
-    #sacamos los valores 
-    inversion = float(fila_pelicula["budget"])
-    ganancia = float(fila_pelicula["revenue"]) - inversion
-    retorno = float(fila_pelicula["return"])
-    anio = int(fila_pelicula["release_year"])
-
-    return {'pelicula':pelicula, 'inversion':inversion, 'ganacia':ganancia,'retorno': retorno, 'anio': anio}
+    indice = data[data['title'] == titulo].index[0]
+    puntuaciones_similares = list(enumerate(cosine_similarities[indice]))
+    puntuaciones_similares = sorted(puntuaciones_similares, key=lambda x: x[1], reverse=True)
+    puntuaciones_similares = puntuaciones_similares[1:6]  # Obtiene las 5 películas más similares
+    
+    indices_peliculas = [i[0] for i in puntuaciones_similares]
+    
+    recomendacion = {'lista recomendada': [data['title'].iloc[i] for i in indices_peliculas]}
+    return recomendacion
